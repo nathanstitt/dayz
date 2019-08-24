5b0544a21c6cb8351da695cb71840174a1200d03
Nathan Stitt <nathan@stitt.org>
2019-08-23 19:04:56 -0500

    update dependencies

--- jest.config.js
+++ jest.config.js
@@ -118,7 +118,9 @@ module.exports = {
     // setupFiles: [],
 
     // The path to a module that runs some code to configure or set up the testing framework before each test
-    setupTestFrameworkScriptFile: "<rootDir>/setup.js",
+    setupFilesAfterEnv: [
+        "<rootDir>/setup.js",
+    ],
 
     // A list of paths to snapshot serializer modules Jest should use for snapshot testing
     // snapshotSerializers: [],
