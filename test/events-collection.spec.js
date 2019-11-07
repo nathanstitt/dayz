c940613f77b4d4127d7d30d6ea8c2f74556af6dd
Norbert Kalácska <hello@kalacskanorbert.me>
2019-11-07 09:31:45 +0100

    Added test for displayLabelForAllDays

--- test/events-collection.spec.js
+++ test/events-collection.spec.js
@@ -17,24 +17,54 @@ describe('Events Collection', () => {
 
     describe('display events as multiple NOT all day events', () => {
         describe('range is more than one day', () => {
-            it('creates events from object', () => {
-                const collection = new EventsCollection(
-                    [{ range: moment.range('2011-10-01', '2011-10-02 12:00:01') }],
-                    { displayAllDay: false },
-                );
-                expect(collection.events.length).toEqual(2);
+            const range = moment.range('2011-10-01', '2011-10-02 12:00:01');
 
-                const event = collection.events[0];
-                expect(event).toEqual(expect.any(Event));
-                expect(event.range().isSame(
-                    moment.range('2011-10-01 00:00:00', '2011-10-01 23:59:59.999'),
-                )).toBe(true);
+            describe('displayLabelForAllDays is any other then false', () => {
+                it('creates events from object', () => {
+                    const collection = new EventsCollection(
+                        [{ content: 'Test Label', range }],
+                        { displayAllDay: false },
+                    );
+                    expect(collection.events.length).toEqual(2);
 
-                const event_b = collection.events[1];
-                expect(event_b).toEqual(expect.any(Event));
-                expect(event_b.range().isSame(
-                    moment.range('2011-10-02 00:00:00', '2011-10-02 12:00:01'),
-                )).toBe(true);
+                    const event = collection.events[0];
+                    expect(event).toEqual(expect.any(Event));
+                    expect(event.range().isSame(
+                        moment.range('2011-10-01 00:00:00', '2011-10-01 23:59:59.999'),
+                    )).toBe(true);
+                    expect(event.content).toBe('Test Label');
+
+                    const event_b = collection.events[1];
+                    expect(event_b).toEqual(expect.any(Event));
+                    expect(event_b.range().isSame(
+                        moment.range('2011-10-02 00:00:00', '2011-10-02 12:00:01'),
+                    )).toBe(true);
+                    expect(event_b.content).toBe('Test Label');
+                });
+            });
+
+            describe('displayLabelForAllDays is false', () => {
+                it('creates events from object', () => {
+                    const collection = new EventsCollection(
+                        [{ content: 'Test Label', range }],
+                        { displayAllDay: false, displayLabelForAllDays: false },
+                    );
+                    expect(collection.events.length).toEqual(2);
+
+                    const event = collection.events[0];
+                    expect(event).toEqual(expect.any(Event));
+                    expect(event.range().isSame(
+                        moment.range('2011-10-01 00:00:00', '2011-10-01 23:59:59.999'),
+                    )).toBe(true);
+                    expect(event.content).toBe('Test Label');
+
+                    const event_b = collection.events[1];
+                    expect(event_b).toEqual(expect.any(Event));
+                    expect(event_b.range().isSame(
+                        moment.range('2011-10-02 00:00:00', '2011-10-02 12:00:01'),
+                    )).toBe(true);
+                    expect(event_b.content).toBe(' ');
+                });
             });
         });
 
